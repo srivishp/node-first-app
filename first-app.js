e480a03e5654b5dcbdda1d64488190ac8db831be
Sri Vishnu Pasumarthi <srivishp@gmail.com>
2025-07-07 13:16:15 +0530

    Use fs module

--- first-app.js
+++ first-app.js
@@ -1,1 +1,6 @@
-console.log('Hello, World!');
+// console.log('Hello, World!');
+
+// Node file system module import
+const fs = require("fs");
+// Writing to a file using fs module
+fs.writeFileSync("hello.txt", "Hello form Node.js");
