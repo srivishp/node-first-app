9bf2404fd8217711ae08c1d40f537bf26fa1b85c
Sri Vishnu <srivishp@gmail.com>
2025-07-08 15:21:45 +0530

    Headers, route & redirect requests

--- app.js
+++ app.js
@@ -5,19 +5,40 @@ const http = require("http");
 
 // Request listener function
 const rqListener = (req, res) => {
-  console.log(req.url, req.method, req.headers);
+  // console.log(req.url, req.method, req.headers);
   // Quits the server if no events are registered
   // process.exit();
   //? But we do not use this, because users will not be able to reach our webpage
 
-  //Sending a response
-  res.setHeader("Content-Type", "text/html");
-  // noob way of sending html content
-  res.write(
-    "<html><head><title>My First Page</title></head><body>Hello from my first Node.js Server!</body></html>"
-  );
-  // After res.end(), we should not write anything or it will give an error
-  res.end();
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    // noob way of sending html content
+    res.write("<html>");
+    res.write("<head><title>Enter message</title></head>");
+    res.write("<body>");
+    res.write("<form action='/message' method='POST'>");
+    res.write(
+      "<input type='text' name='message'/><button type='submit'>Send</button>"
+    );
+    res.write("</form>");
+    res.write("</body>");
+    res.write("</html>");
+    // After res.end(), we should not write anything or it will give an error
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    //Sending a response
+    res.setHeader("Content-Type", "text/html");
+    // noob way of sending html content
+    res.write("<html>");
+    res.write("<head><title>My First Page</title></head>");
+    res.write("<body>Hello from my first Node.js Server!</body>");
+    res.write("</html>");
+    // After res.end(), we should not write anything or it will give an error
+    return res.end();
+  }
 };
 
 // referencing rqListener
