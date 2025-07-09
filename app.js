a076f2e04328befcea60fed6f951dcbb37fdef46
Sri Vishnu Pasumarthi <srivishp@gmail.com>
2025-07-09 20:17:35 +0530

    Use node modules system

--- app.js
+++ app.js
@@ -2,63 +2,18 @@
 
 // "./http" looks for a local file. Only "http" looks for a global module
 const http = require("http");
-const fs = require("fs");
+const routes = require("./routes");
 
 // Request listener function
-const rqListener = (req, res) => {
-  // console.log(req.url, req.method, req.headers);
-  // Quits the server if no events are registered
-  // process.exit();
-  //? But we do not use this, because users will not be able to reach our webpage
-
-  const url = req.url;
-  const method = req.method;
-  if (url === "/") {
-    // noob way of sending html content
-    res.write("<html>");
-    res.write("<head><title>Enter message</title></head>");
-    res.write("<body>");
-    res.write("<form action='/message' method='POST'>");
-    res.write(
-      "<input type='text' name='message'/><button type='submit'>Send</button>"
-    );
-    res.write("</form>");
-    res.write("</body>");
-    res.write("</html>");
-    // After res.end(), we should not write anything or it will give an error
-    return res.end();
-  }
-
-  if (url === "/message" && method === "POST") {
-    // Parsing the request body (Streams & Buffers)
-    const body = [];
-    req.on("data", (chunk) => {
-      console.log(chunk);
-      body.push(chunk);
-    });
-    req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split("=")[1];
-      fs.writeFile("message.txt", message, (err) => {
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        return res.end();
-      });
-    });
-
-    //Sending a response
-    res.setHeader("Content-Type", "text/html");
-
-    res.write("<html>");
-    res.write("<head><title>My First Page</title></head>");
-    res.write("<body>Hello from my first Node.js Server!</body>");
-    res.write("</html>");
-    return res.end();
-  }
-};
+// const rqListener = (req, res) => {
+//   // console.log(req.url, req.method, req.headers);
+//   // Quits the server if no events are registered
+//   // process.exit();
+//   //? But we do not use process.exit(), because users will not be able to reach our webpage
+// };
 
 // referencing rqListener
-const server = http.createServer(rqListener);
+const server = http.createServer(routes);
 
 // Node JS will keep the server running to listen to incoming requests (Event Loop)
 // listen() takes optional arguments. Here, we are using port number 3000.
