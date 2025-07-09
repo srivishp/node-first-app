e44fa61e1872deba47d9422fb7952707d5343f44
Sri Vishnu Pasumarthi <srivishp@gmail.com>
2025-07-09 19:52:50 +0530

    Update comments

--- first-app.js
+++ first-app.js
@@ -2,6 +2,7 @@
 const fs = require("fs");
 // Writing to a file using fs module
 //# writeFileSync is synchronous, writeFile is asynchronous & always better.
+// but for now we use writeFileSync
 fs.writeFileSync("hello.txt", "Hello form Node.js");
 
 console.log("Hello, World!");
