300d69d7f11057f6eba6a0b5ac3e7eb52912697d
Sri Vishnu <srivishp@gmail.com>
2025-07-10 15:02:00 +0530

    Install nodemon, add gitignore file.

--- first-app.js
+++ first-app.js
@@ -1,5 +1,6 @@
 // Node file system module import
 const fs = require("fs");
+
 // Writing to a file using fs module
 //# writeFileSync is synchronous, writeFile is asynchronous & always better.
 // but for now we use writeFileSync
